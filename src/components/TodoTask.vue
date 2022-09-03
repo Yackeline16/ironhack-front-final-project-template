4c299efa75e1b4ecbf4eff3dea972ed7648c0d96
Yackeline16 <jacqueline@capa8.net>
2022-09-03 16:39:38 +0200

    create Task

---
+++ src/components/TodoTask.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="flex mb-4 items-center">
+      <p class="w-full text-grey-900">
+        Add another component to Tailwind Components
+      </p>
+      <button
+        class="
+          flex-no-shrink
+          p-2
+          ml-4
+          mr-2
+          border-2
+          rounded
+          hover:text-white
+          text-green-600
+          border-green-600
+          hover:bg-green-600
+        "
+      >
+        Done
+      </button>
+      <button
+        class="
+          flex-no-shrink
+          p-2
+          ml-2
+          border-2
+          rounded
+          text-red-500
+          border-red-500
+          hover:text-white hover:bg-red-500
+        "
+      >
+        Remove
+      </button>
+    </div>
+    <div class="flex mb-4 items-center">
+      <p class="w-full line-through text-green-600">
+        Submit Todo App Component to Tailwind Components
+      </p>
+      <button
+        class="
+          flex-no-shrink
+          p-2
+          ml-4
+          mr-2
+          border-2
+          rounded
+          hover:text-white
+          text-grey
+          border-gray-400
+          hover:bg-gray-400
+        "
+      >
+        Not Done
+      </button>
+      <button
+        class="
+          flex-no-shrink
+          p-2
+          ml-2
+          border-2
+          rounded
+          text-red-500
+          border-red-500
+          hover:text-white hover:bg-red-500
+        "
+      >
+        Remove
+      </button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,187 +1,64 @@
 <template>
   <NavBar />
-  <section>
-    <header
-      class="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6"
-    >
-      <div class="flex items-center justify-between">
-        <h2 class="font-semibold text-slate-900">TASKS</h2>
-        <a
-          href="/new"
-          class="
-            hover:bg-blue-400
-            group
-            flex
-            items-center
-            rounded-md
-            bg-blue-500
-            text-white text-sm
-            font-medium
-            pl-2
-            pr-3
-            py-2
-            shadow-sm
-          "
-        >
-          <svg
-            width="20"
-            height="20"
-            fill="currentColor"
-            class="mr-2"
-            aria-hidden="true"
-          >
-            <path
-              d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z"
-            />
-          </svg>
-          New
-        </a>
-      </div>
-      <form class="group relative">
-        <svg
-          width="20"
-          height="20"
-          fill="currentColor"
-          class="
-            absolute
-            left-3
-            top-1/2
-            -mt-2.5
-            text-slate-400
-            pointer-events-none
-            group-focus-within:text-blue-500
-          "
-          aria-hidden="true"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-          />
-        </svg>
-        <input
-          class="
-            focus:ring-2 focus:ring-blue-500 focus:outline-none
-            appearance-none
-            w-full
-            text-sm
-            leading-6
-            text-slate-900
-            placeholder-slate-400
-            rounded-md
-            py-2
-            pl-10
-            ring-1 ring-slate-200
-            shadow-sm
-          "
+  <div
+    class="
+      h-100
+      w-full
+      flex
+      items-center
+      justify-center
+      bg-teal-lightest
+      font-sans
+      max-w-
+    "
+  >
+    <div class="bg-white rounded shadow p-6 m-4 w-full lg:max-w-lg">
+      <div class="mb-4">
+        <h1 class="fond-bold text-grey-900">Todo List</h1>
+        <div class="flex mt-4">
+          <input
+          id="todotext"
           type="text"
-          aria-label="Filter tasks"
-          placeholder="Filter tasks..."
-        />
-      </form>
-    </header>
-    <ul
-      class="
-        bg-slate-50
-        p-4
-        sm:px-8 sm:pt-6 sm:pb-8
-        lg:p-4
-        xl:px-8 xl:pt-6 xl:pb-8
-        grid grid-cols-1
-        sm:grid-cols-2
-        lg:grid-cols-1
-        xl:grid-cols-2
-        gap-4
-        text-sm
-        leading-6
-      "
-    >
-      <li x-for="task in tasks">
-        <div
-          
-          class="
-            hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md
-            group
-            rounded-md
-            p-3
-            bg-white
-            ring-1 ring-slate-200
-            shadow-sm
-          "
-        >
-          <dl
             class="
-              grid
-              sm:block
-              lg:grid
-              xl:block
-              grid-cols-2 grid-rows-2
-              items-center
+              shadow
+              appearance-none
+              border
+              rounded
+              w-full
+              py-2
+              px-3
+              mr-4
+              text-cyan-900
+            "
+            placeholder="Add Todo"
+          />
+          <button
+            class="
+              flex-no-shrink
+              p-2
+              border-2
+              rounded
+              text-teal
+              border-teal
+              hover:text-white hover:bg-sky-500
             "
           >
-            <div>
-              <dt class="sr-only">Title</dt>
-              <dd class="group-hover:text-white font-semibold text-slate-900">
-                
-              </dd>
-            </div>
-            <div>
-              <dt class="sr-only">Category</dt>
-              <dd class="group-hover:text-blue-200"></dd>
-            </div>
-            <div
-              class="col-start-2 row-start-1 row-end-3 sm:mt-4 lg:mt-0 xl:mt-4"
-            >
-           
-            </div>
-          </dl>
+            +
+          </button>
         </div>
-      </li>
-      <li class="flex">
-        <input
-          class="
-            hover:border-blue-500
-            hover:border-solid
-            hover:bg-white
-            hover:text-blue-500
-            group
-            w-full
-            flex flex-col
-            items-center
-            justify-center
-            rounded-md
-            border-2 border-dashed border-slate-300
-            text-sm
-            leading-6
-            text-slate-900
-            font-medium
-            py-3
-          "
-        >
-          <svg
-            class="group-hover:text-blue-500 mb-1 text-slate-400"
-            width="20"
-            height="20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z"
-            />
-          </svg>
-          New Task
-        
-      </li>
-    </ul>
-  </section>
+      </div>
+      <div>
+       <TodoTask />
+      </div>
+    </div>
+  </div>
   <footer
     class="
       p-4
       bg-white
       rounded-lg
       shadow
-      md:flex md:items-center md:justify-center
-       md:p-6
+      md:flex md:items-center md:justify-center md:p-6
       dark:bg-gray-800
     "
   >
@@ -194,13 +71,16 @@
 
 <script>
 import NavBar from "../components/NavBar.vue";
-
+import TodoTask from "..components/TodoTask.vue"
 export default {
-  components: { NavBar },
-
-
-}
+  components: { NavBar, TodoTask },
+};
 </script>
 
 <style>
+h1 {
+  font-weight: 600;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 40px;
+}
 </style>
